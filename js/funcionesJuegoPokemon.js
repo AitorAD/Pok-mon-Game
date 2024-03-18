f2d683a149d768cbb21cd4a91ce9724db92a888f
Aitor <aduqueagullo@gmail.com>
2024-03-19 00:04:48 +0100

    Added Pokemon Game

---
+++ js/funcionesJuegoPokemon.js
@@ -0,0 +1,403 @@
+const siluetas = {
+    Bulbasaur: "/img/siluetas/1Silueta.webp",
+    Ivysaur: "/img/siluetas/2Silueta.webp",
+    Venusaur: "/img/siluetas/3Silueta.webp",
+    Charmander: "/img/siluetas/4Silueta.webp",
+    Charmeleon: "/img/siluetas/5Silueta.webp",
+    Charizard: "/img/siluetas/6Silueta.webp",
+    Squirtle: "/img/siluetas/7Silueta.webp",
+    Wartortle: "/img/siluetas/8Silueta.webp",
+    Blastoise: "/img/siluetas/9Silueta.webp",
+    Caterpie: "/img/siluetas/10Silueta.webp",
+    Metapod: "/img/siluetas/11Silueta.webp",
+    Butterfree: "/img/siluetas/12Silueta.webp",
+    Weedle: "/img/siluetas/13Silueta.webp",
+    Kakuna: "/img/siluetas/14Silueta.webp",
+    Beedrill: "/img/siluetas/15Silueta.webp",
+    Pidgey: "/img/siluetas/16Silueta.webp",
+    Pidgeotto: "/img/siluetas/17Silueta.webp",
+    Pidgeot: "/img/siluetas/18Silueta.webp",
+    Rattata: "/img/siluetas/19Silueta.webp",
+    Raticate: "/img/siluetas/20Silueta.webp",
+    Spearow: "/img/siluetas/21Silueta.webp",
+    Fearow: "/img/siluetas/22Silueta.webp",
+    Ekans: "/img/siluetas/23Silueta.webp",
+    Arbok: "/img/siluetas/24Silueta.webp",
+    Pikachu: "/img/siluetas/25Silueta.webp",
+    Raichu: "/img/siluetas/26Silueta.webp",
+    Sandshrew: "/img/siluetas/27Silueta.webp",
+    Sandslash: "/img/siluetas/28Silueta.webp",
+    NidoranHembra: "/img/siluetas/29Silueta.webp",
+    Nidorina: "/img/siluetas/30Silueta.webp",
+    Nidoqueen: "/img/siluetas/31Silueta.webp",
+    NidoranMacho: "/img/siluetas/32Silueta.webp",
+    Nidorino: "/img/siluetas/33Silueta.webp",
+    Nidoking: "/img/siluetas/34Silueta.webp",
+    Clefairy: "/img/siluetas/35Silueta.webp",
+    Clefable: "/img/siluetas/36Silueta.webp",
+    Vulpix: "/img/siluetas/37Silueta.webp",
+    Ninetales: "/img/siluetas/38Silueta.webp",
+    Jigglypuff: "/img/siluetas/39Silueta.webp",
+    Wigglytuff: "/img/siluetas/40Silueta.webp",
+    Zubat: "/img/siluetas/41Silueta.webp",
+    Golbat: "/img/siluetas/42Silueta.webp",
+    Oddish: "/img/siluetas/43Silueta.webp",
+    Gloom: "/img/siluetas/44Silueta.webp",
+    Vileplume: "/img/siluetas/45Silueta.webp",
+    Paras: "/img/siluetas/46Silueta.webp",
+    Parasect: "/img/siluetas/47Silueta.webp",
+    Venonat: "/img/siluetas/48Silueta.webp",
+    Venomoth: "/img/siluetas/49Silueta.webp",
+    Diglett: "/img/siluetas/50Silueta.webp",
+    Dugtrio: "/img/siluetas/51Silueta.webp",
+    Meowth: "/img/siluetas/52Silueta.webp",
+    Persian: "/img/siluetas/53Silueta.webp",
+    Psyduck: "/img/siluetas/54Silueta.webp",
+    Golduck: "/img/siluetas/55Silueta.webp",
+    Mankey: "/img/siluetas/56Silueta.webp",
+    Primeape: "/img/siluetas/57Silueta.webp",
+    Growlithe: "/img/siluetas/58Silueta.webp",
+    Arcanine: "/img/siluetas/59Silueta.webp",
+    Poliwag: "/img/siluetas/60Silueta.webp",
+    Poliwhirl: "/img/siluetas/61Silueta.webp",
+    Poliwrath: "/img/siluetas/62Silueta.webp",
+    Abra: "/img/siluetas/63Silueta.webp",
+    Kadabra: "/img/siluetas/64Silueta.webp",
+    Alakazam: "/img/siluetas/65Silueta.webp",
+    Machop: "/img/siluetas/66Silueta.webp",
+    Machoke: "/img/siluetas/67Silueta.webp",
+    Machamp: "/img/siluetas/68Silueta.webp",
+    Bellsprout: "/img/siluetas/69Silueta.webp",
+    Weepinbell: "/img/siluetas/70Silueta.webp",
+    Victreebel: "/img/siluetas/71Silueta.webp",
+    Tentacool: "/img/siluetas/72Silueta.webp",
+    Tentacruel: "/img/siluetas/73Silueta.webp",
+    Geodude: "/img/siluetas/74Silueta.webp",
+    Graveler: "/img/siluetas/75Silueta.webp",
+    Golem: "/img/siluetas/76Silueta.webp",
+    Ponyta: "/img/siluetas/77Silueta.webp",
+    Rapidash: "/img/siluetas/78Silueta.webp",
+    Slowpoke: "/img/siluetas/79Silueta.webp",
+    Slowbro: "/img/siluetas/80Silueta.webp",
+    Magnemite: "/img/siluetas/81Silueta.webp",
+    Magneton: "/img/siluetas/82Silueta.webp",
+    Farfetch: "/img/siluetas/83Silueta.webp",
+    Doduo: "/img/siluetas/84Silueta.webp",
+    Dodrio: "/img/siluetas/85Silueta.webp",
+    Seel: "/img/siluetas/86Silueta.webp",
+    Dewgong: "/img/siluetas/87Silueta.webp",
+    Grimer: "/img/siluetas/88Silueta.webp",
+    Muk: "/img/siluetas/89Silueta.webp",
+    Shellder: "/img/siluetas/90Silueta.webp",
+    Cloyster: "/img/siluetas/91Silueta.webp",
+    Gastly: "/img/siluetas/92Silueta.webp",
+    Haunter: "/img/siluetas/93Silueta.webp",
+    Gengar: "/img/siluetas/94Silueta.webp",
+    Onix: "/img/siluetas/95Silueta.webp",
+    Drowzee: "/img/siluetas/96Silueta.webp",
+    Hypno: "/img/siluetas/97Silueta.webp",
+    Krabby: "/img/siluetas/98Silueta.webp",
+    Kingler: "/img/siluetas/99Silueta.webp",
+    Voltorb: "/img/siluetas/100Silueta.webp",
+    Electrode: "/img/siluetas/101Silueta.webp",
+    Exeggcute: "/img/siluetas/102Silueta.webp",
+    Exeggutor: "/img/siluetas/103Silueta.webp",
+    Cubone: "/img/siluetas/104Silueta.webp",
+    Marowak: "/img/siluetas/105Silueta.webp",
+    Hitmonlee: "/img/siluetas/106Silueta.webp",
+    Hitmonchan: "/img/siluetas/107Silueta.webp",
+    Lickitung: "/img/siluetas/108Silueta.webp",
+    Koffing: "/img/siluetas/109Silueta.webp",
+    Weezing: "/img/siluetas/110Silueta.webp",
+    Rhyhorn: "/img/siluetas/111Silueta.webp",
+    Rhydon: "/img/siluetas/112Silueta.webp",
+    Chansey: "/img/siluetas/113Silueta.webp",
+    Tangela: "/img/siluetas/114Silueta.webp",
+    Kangaskhan: "/img/siluetas/115Silueta.webp",
+    Horsea: "/img/siluetas/116Silueta.webp",
+    Seadra: "/img/siluetas/117Silueta.webp",
+    Goldeen: "/img/siluetas/118Silueta.webp",
+    Seaking: "/img/siluetas/119Silueta.webp",
+    Staryu: "/img/siluetas/120Silueta.webp",
+    Starmie: "/img/siluetas/121Silueta.webp",
+    MrMime: "/img/siluetas/122Silueta.webp",
+    Scyther: "/img/siluetas/123Silueta.webp",
+    Jynx: "/img/siluetas/124Silueta.webp",
+    Electabuzz: "/img/siluetas/125Silueta.webp",
+    Magmar: "/img/siluetas/126Silueta.webp",
+    Pinsir: "/img/siluetas/127Silueta.webp",
+    Tauros: "/img/siluetas/128Silueta.webp",
+    Magikarp: "/img/siluetas/129Silueta.webp",
+    Gyarados: "/img/siluetas/130Silueta.webp",
+    Lapras: "/img/siluetas/131Silueta.webp",
+    Ditto: "/img/siluetas/132Silueta.webp",
+    Eevee: "/img/siluetas/133Silueta.webp",
+    Vaporeon: "/img/siluetas/134Silueta.webp",
+    Jolteon: "/img/siluetas/135Silueta.webp",
+    Flareon: "/img/siluetas/136Silueta.webp",
+    Porygon: "/img/siluetas/137Silueta.webp",
+    Omanyte: "/img/siluetas/138Silueta.webp",
+    Omastar: "/img/siluetas/139Silueta.webp",
+    Kabuto: "/img/siluetas/140Silueta.webp",
+    Kabutops: "/img/siluetas/141Silueta.webp",
+    Aerodactyl: "/img/siluetas/142Silueta.webp",
+    Snorlax: "/img/siluetas/143Silueta.webp",
+    Articuno: "/img/siluetas/144Silueta.webp",
+    Zapdos: "/img/siluetas/145Silueta.webp",
+    Moltres: "/img/siluetas/146Silueta.webp",
+    Dratini: "/img/siluetas/147Silueta.webp",
+    Dragonair: "/img/siluetas/148Silueta.webp",
+    Dragonite: "/img/siluetas/149Silueta.webp",
+    Mewtwo: "/img/siluetas/150Silueta.webp",
+    Mew: "/img/siluetas/151Silueta.webp",
+};
+
+const pokemon = {
+    Bulbasaur: "/img/pokemon/1Pokemon.webp",
+    Ivysaur: "/img/pokemon/2Pokemon.webp",
+    Venusaur: "/img/pokemon/3Pokemon.webp",
+    Charmander: "/img/pokemon/4Pokemon.webp",
+    Charmeleon: "/img/pokemon/5Pokemon.webp",
+    Charizard: "/img/pokemon/6Pokemon.webp",
+    Squirtle: "/img/pokemon/7Pokemon.webp",
+    Wartortle: "/img/pokemon/8Pokemon.webp",
+    Blastoise: "/img/pokemon/9Pokemon.webp",
+    Caterpie: "/img/pokemon/10Pokemon.webp",
+    Metapod: "/img/pokemon/11Pokemon.webp",
+    Butterfree: "/img/pokemon/12Pokemon.webp",
+    Weedle: "/img/pokemon/13Pokemon.webp",
+    Kakuna: "/img/pokemon/14Pokemon.webp",
+    Beedrill: "/img/pokemon/15Pokemon.webp",
+    Pidgey: "/img/pokemon/16Pokemon.webp",
+    Pidgeotto: "/img/pokemon/17Pokemon.webp",
+    Pidgeot: "/img/pokemon/18Pokemon.webp",
+    Rattata: "/img/pokemon/19Pokemon.webp",
+    Raticate: "/img/pokemon/20Pokemon.webp",
+    Spearow: "/img/pokemon/21Pokemon.webp",
+    Fearow: "/img/pokemon/22Pokemon.webp",
+    Ekans: "/img/pokemon/23Pokemon.webp",
+    Arbok: "/img/pokemon/24Pokemon.webp",
+    Pikachu: "/img/pokemon/25Pokemon.webp",
+    Raichu: "/img/pokemon/26Pokemon.webp",
+    Sandshrew: "/img/pokemon/27Pokemon.webp",
+    Sandslash: "/img/pokemon/28Pokemon.webp",
+    NidoranHembra: "/img/pokemon/29Pokemon.webp",
+    Nidorina: "/img/pokemon/30Pokemon.webp",
+    Nidoqueen: "/img/pokemon/31Pokemon.webp",
+    NidoranMacho: "/img/pokemon/32Pokemon.webp",
+    Nidorino: "/img/pokemon/33Pokemon.webp",
+    Nidoking: "/img/pokemon/34Pokemon.webp",
+    Clefairy: "/img/pokemon/35Pokemon.webp",
+    Clefable: "/img/pokemon/36Pokemon.webp",
+    Vulpix: "/img/pokemon/37Pokemon.webp",
+    Ninetales: "/img/pokemon/38Pokemon.webp",
+    Jigglypuff: "/img/pokemon/39Pokemon.webp",
+    Wigglytuff: "/img/pokemon/40Pokemon.webp",
+    Zubat: "/img/pokemon/41Pokemon.webp",
+    Golbat: "/img/pokemon/42Pokemon.webp",
+    Oddish: "/img/pokemon/43Pokemon.webp",
+    Gloom: "/img/pokemon/44Pokemon.webp",
+    Vileplume: "/img/pokemon/45Pokemon.webp",
+    Paras: "/img/pokemon/46Pokemon.webp",
+    Parasect: "/img/pokemon/47Pokemon.webp",
+    Venonat: "/img/pokemon/48Pokemon.webp",
+    Venomoth: "/img/pokemon/49Pokemon.webp",
+    Diglett: "/img/pokemon/50Pokemon.webp",
+    Dugtrio: "/img/pokemon/51Pokemon.webp",
+    Meowth: "/img/pokemon/52Pokemon.webp",
+    Persian: "/img/pokemon/53Pokemon.webp",
+    Psyduck: "/img/pokemon/54Pokemon.webp",
+    Golduck: "/img/pokemon/55Pokemon.webp",
+    Mankey: "/img/pokemon/56Pokemon.webp",
+    Primeape: "/img/pokemon/57Pokemon.webp",
+    Growlithe: "/img/pokemon/58Pokemon.webp",
+    Arcanine: "/img/pokemon/59Pokemon.webp",
+    Poliwag: "/img/pokemon/60Pokemon.webp",
+    Poliwhirl: "/img/pokemon/61Pokemon.webp",
+    Poliwrath: "/img/pokemon/62Pokemon.webp",
+    Abra: "/img/pokemon/63Pokemon.webp",
+    Kadabra: "/img/pokemon/64Pokemon.webp",
+    Alakazam: "/img/pokemon/65Pokemon.webp",
+    Machop: "/img/pokemon/66Pokemon.webp",
+    Machoke: "/img/pokemon/67Pokemon.webp",
+    Machamp: "/img/pokemon/68Pokemon.webp",
+    Bellsprout: "/img/pokemon/69Pokemon.webp",
+    Weepinbell: "/img/pokemon/70Pokemon.webp",
+    Victreebel: "/img/pokemon/71Pokemon.webp",
+    Tentacool: "/img/pokemon/72Pokemon.webp",
+    Tentacruel: "/img/pokemon/73Pokemon.webp",
+    Geodude: "/img/pokemon/74Pokemon.webp",
+    Graveler: "/img/pokemon/75Pokemon.webp",
+    Golem: "/img/pokemon/76Pokemon.webp",
+    Ponyta: "/img/pokemon/77Pokemon.webp",
+    Rapidash: "/img/pokemon/78Pokemon.webp",
+    Slowpoke: "/img/pokemon/79Pokemon.webp",
+    Slowbro: "/img/pokemon/80Pokemon.webp",
+    Magnemite: "/img/pokemon/81Pokemon.webp",
+    Magneton: "/img/pokemon/82Pokemon.webp",
+    Farfetch: "/img/pokemon/83Pokemon.webp",
+    Doduo: "/img/pokemon/84Pokemon.webp",
+    Dodrio: "/img/pokemon/85Pokemon.webp",
+    Seel: "/img/pokemon/86Pokemon.webp",
+    Dewgong: "/img/pokemon/87Pokemon.webp",
+    Grimer: "/img/pokemon/88Pokemon.webp",
+    Muk: "/img/pokemon/89Pokemon.webp",
+    Shellder: "/img/pokemon/90Pokemon.webp",
+    Cloyster: "/img/pokemon/91Pokemon.webp",
+    Gastly: "/img/pokemon/92Pokemon.webp",
+    Haunter: "/img/pokemon/93Pokemon.webp",
+    Gengar: "/img/pokemon/94Pokemon.webp",
+    Onix: "/img/pokemon/95Pokemon.webp",
+    Drowzee: "/img/pokemon/96Pokemon.webp",
+    Hypno: "/img/pokemon/97Pokemon.webp",
+    Krabby: "/img/pokemon/98Pokemon.webp",
+    Kingler: "/img/pokemon/99Pokemon.webp",
+    Voltorb: "/img/pokemon/100Pokemon.webp",
+    Electrode: "/img/pokemon/101Pokemon.webp",
+    Exeggcute: "/img/pokemon/102Pokemon.webp",
+    Exeggutor: "/img/pokemon/103Pokemon.webp",
+    Cubone: "/img/pokemon/104Pokemon.webp",
+    Marowak: "/img/pokemon/105Pokemon.webp",
+    Hitmonlee: "/img/pokemon/106Pokemon.webp",
+    Hitmonchan: "/img/pokemon/107Pokemon.webp",
+    Lickitung: "/img/pokemon/108Pokemon.webp",
+    Koffing: "/img/pokemon/109Pokemon.webp",
+    Weezing: "/img/pokemon/110Pokemon.webp",
+    Rhyhorn: "/img/pokemon/111Pokemon.webp",
+    Rhydon: "/img/pokemon/112Pokemon.webp",
+    Chansey: "/img/pokemon/113Pokemon.webp",
+    Tangela: "/img/pokemon/114Pokemon.webp",
+    Kangaskhan: "/img/pokemon/115Pokemon.webp",
+    Horsea: "/img/pokemon/116Pokemon.webp",
+    Seadra: "/img/pokemon/117Pokemon.webp",
+    Goldeen: "/img/pokemon/118Pokemon.webp",
+    Seaking: "/img/pokemon/119Pokemon.webp",
+    Staryu: "/img/pokemon/120Pokemon.webp",
+    Starmie: "/img/pokemon/121Pokemon.webp",
+    MrMime: "/img/pokemon/122Pokemon.webp",
+    Scyther: "/img/pokemon/123Pokemon.webp",
+    Jynx: "/img/pokemon/124Pokemon.webp",
+    Electabuzz: "/img/pokemon/125Pokemon.webp",
+    Magmar: "/img/pokemon/126Pokemon.webp",
+    Pinsir: "/img/pokemon/127Pokemon.webp",
+    Tauros: "/img/pokemon/128Pokemon.webp",
+    Magikarp: "/img/pokemon/129Pokemon.webp",
+    Gyarados: "/img/pokemon/130Pokemon.webp",
+    Lapras: "/img/pokemon/131Pokemon.webp",
+    Ditto: "/img/pokemon/132Pokemon.webp",
+    Eevee: "/img/pokemon/133Pokemon.webp",
+    Vaporeon: "/img/pokemon/134Pokemon.webp",
+    Jolteon: "/img/pokemon/135Pokemon.webp",
+    Flareon: "/img/pokemon/136Pokemon.webp",
+    Porygon: "/img/pokemon/137Pokemon.webp",
+    Omanyte: "/img/pokemon/138Pokemon.webp",
+    Omastar: "/img/pokemon/139Pokemon.webp",
+    Kabuto: "/img/pokemon/140Pokemon.webp",
+    Kabutops: "/img/pokemon/141Pokemon.webp",
+    Aerodactyl: "/img/pokemon/142Pokemon.webp",
+    Snorlax: "/img/pokemon/143Pokemon.webp",
+    Articuno: "/img/pokemon/144Pokemon.webp",
+    Zapdos: "/img/pokemon/145Pokemon.webp",
+    Moltres: "/img/pokemon/146Pokemon.webp",
+    Dratini: "/img/pokemon/147Pokemon.webp",
+    Dragonair: "/img/pokemon/148Pokemon.webp",
+    Dragonite: "/img/pokemon/149Pokemon.webp",
+    Mewtwo: "/img/pokemon/150Pokemon.webp",
+    Mew: "/img/pokemon/151Pokemon.webp",
+};
+
+let totalPokemonAcertados = 0;
+let vidas = 3;
+let racha = 0;
+let seguirJugando;
+let posicionPokemon;
+
+function generarSiluetaPokemon() {
+    let claves = Object.keys(siluetas);
+    let numRandom = Math.floor(Math.random() * claves.length);
+    let imgOutput = document.getElementById('pokemon');
+
+    document.querySelector('figcaption').textContent = "";
+
+    imgOutput.src = siluetas[claves[numRandom]];
+
+    for (let key in siluetas) {
+        if (siluetas.hasOwnProperty(key)) {
+            if (siluetas[claves[numRandom]] === siluetas[key]) {
+                posicionPokemon = key;
+                delete siluetas[claves[numRandom]];
+            }
+        }
+    }
+    seguirJugando = false;
+}
+
+function anyadirImgPokemon(id) {
+    let imgOutput = document.getElementById('pokemon');
+    let clavesSiluetas = Object.keys(siluetas);
+    let clavesPokemon = Object.keys(pokemon);
+
+    if (clavesSiluetas.length === 0) {
+        imgOutput.src = "/img/trofeo.webp";
+        document.querySelector('figcaption').textContent = "¡Enhorabuena, has ganado!. Has entrado en el hall de la fama.";
+    } else {
+        let pokemonInput = document.getElementById(id).value;
+
+        if (pokemonInput === posicionPokemon) {
+            imgOutput.src = pokemon[pokemonInput];
+            document.querySelector('figcaption').textContent = "¡Es " + posicionPokemon + "!";
+            racha++;
+            if (racha > 5) {
+                totalPokemonAcertados += 2;
+                let numRandom = Math.floor(Math.random() * clavesSiluetas.length);
+                delete siluetas[clavesSiluetas[numRandom]];
+                document.getElementById('racha').textContent = "¡Estás en racha! Tu racha es de: " + racha;
+                document.getElementById('rachaImg').style.visibility = 'visible';
+            } else {
+                totalPokemonAcertados += 1;
+            }
+        } else {
+            document.querySelector('figcaption').textContent = "Vaya... El Pokémon era " + posicionPokemon;
+            document.getElementById('racha').textContent = "";
+            document.getElementById('rachaImg').style.visibility = 'hidden';
+            restarVidas();
+            racha = 0;
+            imgOutput.src = "/img/error.webp";
+            if (vidas === 0) {
+                document.body.style.background = 'red';
+                imgOutput.setAttribute('src', "/img/game_over.webp");
+            }
+        }
+        posicionPokemon = null;
+        seguirJugando = true;
+    }
+    if (clavesSiluetas.length === 0 && totalPokemonAcertados === clavesPokemon.length - 1) totalPokemonAcertados += 1;
+    document.getElementById('totalAciertos').textContent = totalPokemonAcertados;
+}
+
+function mostrarMenuAyuda() {
+    let menuAyuda = document.querySelector('aside');
+
+    if (menuAyuda.style.display === 'none') {
+        menuAyuda.style.display = 'flex';
+    } else {
+        menuAyuda.style.display = 'none';
+    }
+}
+
+function continuar() {
+    if (seguirJugando && vidas > 0) {
+        generarSiluetaPokemon();
+    }
+}
+
+function nuevaPartida() {
+    location.href = "./PokemonGame.html";
+}
+
+function restarVidas() {
+    let vida = document.getElementById('vida' + vidas);
+    vida.setAttribute('src', "/img/corazonVacio.webp")
+    vida.style.animationName = 'none'
+    vidas--;
+}
